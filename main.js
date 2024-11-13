d7e025fa91cda01b0e3e51d363601b5271c6f20a
elcodificad0r <guido.gueta@gmail.com>
2024-11-13 01:12:06 -0600

    aranges

--- main.js
+++ main.js
@@ -6,34 +6,32 @@ const products = [
     { 
         name: "La Sportiva Solution", 
         price: 120, 
-        type: "Boulder", 
-        size: [38, 39, 40, 41, 42] 
+        type: "Boulder",  
     },
     { 
         name: "Scarpa Instinct VS", 
         price: 150, 
-        type: "Deportivo", 
-        size: [37, 38, 39, 40, 41] 
+        type: "Deportivo",  
     },
     { 
         name: "Five Ten Anasazi", 
         price: 100, 
         type: "Trad", 
-        size: [38, 39, 40, 41, 42] 
     },
     { 
         name: "Evolv Shaman", 
         price: 130, 
         type: "Boulder", 
-        size: [39, 40, 41, 42, 43] 
     },
     { 
         name: "Butora Acro", 
         price: 110, 
         type: "Deportivo", 
-        size: [37, 38, 39, 40, 41] 
     }
 ];
+
+let carrito = []
+
 
 function showProducts() {
     let message = "Productos de Climbing Shoes:\n\n";
@@ -42,7 +40,65 @@ function showProducts() {
     });
     message += "\n Para agregar sus productos al carrito regrese al menú principal y seleccione opción 2.";
     alert(message);
+}
 
+// seleccionar producto
+function productSelection (){
+    let userSelection = Number(prompt ("elige el numero" ))
+    console.log(userSelection)
+    carrito.push(products[userSelection - 1])
+    console.table(carrito)
+    return userSelection;
+
+
+}
+//     if (userSelection > 0 && userSelection <= products.length) {
+//         let selectedProduct = products[userSelection - 1];
+//         alert(`Has seleccionado: ${selectedProduct.name} - Tipo: ${selectedProduct.type} - Precio: $${selectedProduct.price}`);
+//         // Aquí podrías agregar el producto al carrito, si existe una estructura de carrito
+//     } else {
+//         alert("Selección no válida, intenta nuevamente.");
+//     }
+// }
+
+function showCarrito() {
+    let message = "Productos de Climbing Shoes:\n\n";
+    carrito.forEach((product, index) => {
+        message += `${index + 1}. ${product.name} - Tipo: ${product.type} - Precio: $${product.price}\n`;
+    });
+    message += "\n Esta es tu selección actual";
+    alert(message);
+}
+
+function editCarrito () {
+    let message = "Carrito, Indique con un número el artículo que desea eliminar:\n\n";
+    carrito.forEach((product, index) => {
+        message += `${index + 1}. ${product.name} - Tipo: ${product.type} - Precio: $${product.price}\n`;
+    });
+    message += "\n Esta es tu selección actual";
+    let userErase = Number(prompt(message));
+    carrito.splice(userErase - 1,1)
+    console.table(carrito)
+    return userErase;
+}
+ 
+function checkout(){
+    let priceTotal = 0
+    let message = "Carrito:\n\n";
+    carrito.forEach((product) => {
+        // message += `${index + 1}. ${product.name} - Tipo: ${product.type} - Precio: $${product.price}\n`;
+        priceTotal += product.price
+    });
+
+
+    let userCheckout = confirm(message + "Este es su total: " + priceTotal + " ¿desea finalizar con su compra?");
+    if (userCheckout == true) {
+        carrito.length = 0
+        alert("Gracias por su compra")
+    }
+    console.log(priceTotal)
+    console.log(carrito)
+    return userCheckout;
 }
 
 function initFunction() {
@@ -51,21 +107,26 @@ function initFunction() {
         while (flag == true) {
             switch (option) {
                 case 1:
-                    // alert("Productos de Climbing Shoes:\n"+ products)
                     showProducts();
                     flag = confirm("¿Quieres seguir operando?")
                 break;
                 case 2:
+                    productSelection();
                     flag = confirm("¿Quieres seguir operando?")
-                break;
+                break; 
                 case 3:
+                    showCarrito();
                     flag = confirm("¿Quieres seguir operando?")
                 break;
                 case 4:
+                    editCarrito();
                     flag = confirm("¿Quieres seguir operando?")
                 break;
                     
-            
+                case 5:
+                    checkout();
+                    flag = confirm("¿Quieres seguir operando?")
+                break;
                 default:
                     alert("No existe esta opción")
                     flag = confirm("¿Quieres seguir operando?")
